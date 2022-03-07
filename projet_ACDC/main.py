5a8d91d1df6471143964c3643abf7bec33436a20
dayotNicolas <nicolas.dayot@ynov.com>
2022-03-07 12:30:15 +0100

    first test pytest ci

---
+++ projet_ACDC/main.py
@@ -0,0 +1,7 @@
+#test du linter lol
+
+def hello_world():
+    print ( "Hello World" )
+    
+hello_world()
+a =5;
---
+++ tests/test_add.py
@@ -0,0 +1,4 @@
+import add_function
+
+def test_add():
+    assert add_function.add(3, 5) == 8
