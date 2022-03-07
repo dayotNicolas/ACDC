79947f447859b2e2d92b9bb7408548708d5db521
f <f>
2022-03-07 10:15:49 +0100

    init fork

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
-#test du linter lol
+"""
+* To install FastApi
+-> pip install fastapi
 
-def hello_world():
-    print ( "Hello World" )
-    
-hello_world()
-a =5;
+* To install ASGI server
+-> pip install "uvicorn[standard]"
+
+* To run uvicorn server
+-> uvicorn main:app --reload
+    The command uvicorn main:app refers to:
+       - main: the file main.py (the Python "module").
+       - app: the object created inside of main.py with the line app = FastAPI().
+       - --reload: make the server restart after code changes. Only do this for development.
+
+* Access to generate doc
+-> http://127.0.0.1:8000/docs.
+
+* To install SQLAlchemy
+-> pip install SQLAlchemy
+
+* Package to install :
+    - FastAPI
+    - sqlalchemy
+"""
+
+from typing import Optional
+from fastapi import FastAPI
+import sqlalchemy
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
