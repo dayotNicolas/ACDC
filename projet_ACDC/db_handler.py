11b4d1a348b67345c07ab8cf5e502f0a22219578
syadone@gmail.com <syadone@gmail.com>
2022-03-28 11:48:41 +0200

    bdhanlder

--- projet_ACDC/db_handler.py
+++ projet_ACDC/db_handler.py
@@ -1,27 +1,32 @@
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, ForeignKey
 
-engine = create_engine('sqlite:///NOM.db', echo = True)
+engine = create_engine('sqlite:///NOM.db', echo=True)
 meta = MetaData()
 
 departments = Table(
-   'departments',
-   meta,
-   Column('id', Integer, primary_key = True),
-   Column('name', String),
-   Column('number', String),
-   Column('death', Integer),
-   Column('icu_occupation', Integer),
-   Column('vaccination', Integer),
-   Column('positivity_rate', Integer)
+    'departments',
+    meta,
+    Column('id', Integer, primary_key=True),
+    Column('name', String),
+    Column('number', String),
+    Column('death', Integer),
+    Column('icu_occupation', Integer),
+    Column('vaccination', Integer),
+    Column('positivity_rate', Integer)
 )
 
-
 addresses = Table(
-   'department_list', meta,
-   Column('id', Integer, primary_key = True),
-   Column('dpt_id', Integer, ForeignKey('departments.id')),
-   Column('dpt_number', String),
-   Column('region', String))
+    'department_list', meta,
+    Column('id', Integer, primary_key=True),
+    Column('dpt_id', Integer, ForeignKey('departments.id')),
+    Column('dpt_number', String),
+    Column('region', String))
+
+ins = departments.insert()
+for i in list:
+    ins = departments.insert().values(name=f'{i}', number=f'{i}',
+                                      death=f'{i}', icu_occupation=f'{i}',
+                                      vaccination=f'{i}', positivity_rate=f'{i}')
 
 meta.create_all(engine)
 
