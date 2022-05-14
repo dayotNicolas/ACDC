e24833d4e1d9cd5d76f649c0e8f0b7c52ef067c5
syadone@gmail.com <syadone@gmail.com>
2022-05-14 14:11:52 +0200

    linted

--- project_ACDC/api_handler.py
+++ project_ACDC/api_handler.py
@@ -1,7 +1,7 @@
 import csv
 import json
 import datetime
-from app import models
+import models
 from database import SessionLocal, engine
 import pandas as pd
 
@@ -16,7 +16,7 @@ def create_database():
         db_record = models.Region(
             id=Column["department_number"],
             region_number=Column["department_number"],
-            name=Column["name"]
+            name=Column["name"],
         )
         db.add(db_record)
     db.commit()
@@ -29,15 +29,15 @@ def middleware_deces_age():
     db = SessionLocal()
 
     models.Base.metadata.create_all(bind=engine)
-    with open('datas/JSON/deces_age_region.json') as json_file:
+    with open("datas/JSON/deces_age_region.json") as json_file:
         data = json.load(json_file)
         for Column in data.items():
-            print(Column[0], type(int(Column[1]['reg'])))
+            print(Column[0], type(int(Column[1]["reg"])))
             db_record = models.DeathAgeRegion(
                 region_id=int(Column[1]["reg"]),
                 cl_age90=int(Column[1]["cl_age90"]),
                 deaths_covid=int(Column[1]["Dc_Elec_Covid_cum"]),
-                date=pd.to_datetime(Column[1]["jour"])
+                date=pd.to_datetime(Column[1]["jour"]),
             )
             db.add(db_record)
         db.commit()
@@ -50,15 +50,15 @@ def middleware_hospitalisation_age():
     db = SessionLocal()
 
     models.Base.metadata.create_all(bind=engine)
-    with open('datas/JSON/hospitalisation_age.json') as json_file:
+    with open("datas/JSON/hospitalisation_age.json") as json_file:
         data = json.load(json_file)
         for Column in data.items():
-            print(Column[0], type(int(Column[1]['reg'])))
+            print(Column[0], type(int(Column[1]["reg"])))
             db_record = models.DeathAgeRegion(
                 region_id=int(Column[1]["reg"]),
                 week=str(Column[1]["Semaine"]),
                 cl_age90=int(Column[1]["cl_age90"]),
-                new_admission_hospitals=int(Column[1]["NewAdmHospit"])
+                new_admission_hospitals=int(Column[1]["NewAdmHospit"]),
             )
             db.add(db_record)
         db.commit()
@@ -71,16 +71,16 @@ def middleware_professional_vaccin_percentage_department():
     db = SessionLocal()
 
     models.Base.metadata.create_all(bind=engine)
-    with open('datas/JSON/vaccin_pro_poucentage_dep.json') as json_file:
+    with open("datas/JSON/vaccin_pro_poucentage_dep.json") as json_file:
         data = json.load(json_file)
         for Column in data.items():
-            print(Column[0], type(int(Column[1]['reg'])))
+            print(Column[0], type(int(Column[1]["reg"])))
             db_record = models.DeathAgeRegion(
                 region_id=int(Column[1]["dep"]),
                 date=pd.to_datetime(Column[1]["jour"]),
                 dose1=float(Column[1]["pro_couv_dose1"]),
                 dose_completed=float(Column[1]["pro_couv_complet"]),
-                dose_rappel=float(Column[1]["pro_couv_rappel"])
+                dose_rappel=float(Column[1]["pro_couv_rappel"]),
             )
             db.add(db_record)
         db.commit()
@@ -93,10 +93,10 @@ def middleware_vaccination_rate():
     db = SessionLocal()
 
     models.Base.metadata.create_all(bind=engine)
-    with open('datas/JSON/vaccin_cage_date.json') as json_file:
+    with open("datas/JSON/vaccin_cage_date.json") as json_file:
         data = json.load(json_file)
         for Column in data.items():
-            print(Column[0], type(int(Column[1]['reg'])))
+            print(Column[0], type(int(Column[1]["reg"])))
             db_record = models.DeathAgeRegion(
                 region_id=int(Column[1]["dep"]),
                 vaccin_number=int(Column[1]["vaccin"]),
@@ -111,7 +111,6 @@ def middleware_vaccination_rate():
                 n_cum_dose3=int(Column[1]["n_cum_dose3"]),
                 n_cum_dose4=int(Column[1]["n_cum_dose4"]),
                 n_cum_rappel=int(Column[1]["n_cum_rappel"]),
-
             )
             db.add(db_record)
         db.commit()
--- project_ACDC/main.py
+++ project_ACDC/main.py
@@ -6,8 +6,10 @@ from api_handler import *
 p = Popen("uvicorn project_ACDC.app:app ", shell=True, stdout=PIPE)
 
 stderr = p.communicate()
-if __name__ == '__main__':
+if __name__ == "__main__":
     schedule.every().day.at("00:00").do(middleware_deces_age)
-    schedule.every().day.at("00:00").do(middleware_professional_vaccin_percentage_department)
+    schedule.every().day.at("00:00").do(
+        middleware_professional_vaccin_percentage_department
+    )
     schedule.every().day.at("00:00").do(middleware_hospitalisation_age)
     schedule.every().day.at("00:00").do(middleware_vaccination_rate)
